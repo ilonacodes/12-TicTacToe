eab37aef171c753b138f5b2e12dc6eae41f52936
Ilona Demidenko <demiluri@gmail.com>
2017-11-26 11:17:23 +0100

    Implement Tic Tac Toe

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 (function () {
-    function Player(sign) {
+    function Player(sign, isComputer) {
         this.sign = sign;
+        this.isComputer = isComputer;
     }
 
-    var playerX = new Player("X");
-    var playerO = new Player("O");
+    var playerX = new Player("X", false);
+    var playerO = new Player("O", true);
     var activePlayer = playerX;
     var gameEnded = false;
 
@@ -36,71 +37,110 @@
 
     function checkWin() {
         if (getCell(0, 0) === playerX.sign && getCell(1, 0) === playerX.sign && getCell(2, 0) === playerX.sign) {
-            return endGame("PlayerX won!");
+            return endGame("Player X won!");
         }
 
         if (getCell(0, 0) === playerO.sign && getCell(1, 0) === playerO.sign && getCell(2, 0) === playerO.sign) {
-            return endGame("PlayerO won!");
+            return endGame("Player O won!");
         }
 
         if (getCell(0, 1) === playerX.sign && getCell(1, 1) === playerX.sign && getCell(2, 1) === playerX.sign) {
-            return endGame("PlayerX won!");
+            return endGame("Player X won!");
         }
 
         if (getCell(0, 1) === playerO.sign && getCell(1, 1) === playerO.sign && getCell(2, 1) === playerO.sign) {
-            return endGame("PlayerO won!");
+            return endGame("Player O won!");
         }
 
         if (getCell(0, 2) === playerX.sign && getCell(1, 2) === playerX.sign && getCell(2, 2) === playerX.sign) {
-            return endGame("PlayerX won!");
+            return endGame("Player X won!");
         }
 
         if (getCell(0, 2) === playerO.sign && getCell(1, 2) === playerO.sign && getCell(2, 2) === playerO.sign) {
-            return endGame("PlayerO won!");
+            return endGame("Player O won!");
         }
 
         if (getCell(0, 0) === playerX.sign && getCell(0, 1) === playerX.sign && getCell(0, 2) === playerX.sign) {
-            return endGame("PlayerX won!");
+            return endGame("Player X won!");
         }
 
         if (getCell(0, 0) === playerO.sign && getCell(0, 1) === playerO.sign && getCell(0, 2) === playerO.sign) {
-            return endGame("PlayerO won!");
+            return endGame("Player O won!");
         }
 
         if (getCell(1, 0) === playerX.sign && getCell(1, 1) === playerX.sign && getCell(1, 2) === playerX.sign) {
-            return endGame("PlayerX won!");
+            return endGame("Player X won!");
         }
 
         if (getCell(1, 0) === playerO.sign && getCell(1, 1) === playerO.sign && getCell(1, 2) === playerO.sign) {
-            return endGame("PlayerO won!");
+            return endGame("Player O won!");
         }
 
         if (getCell(2, 0) === playerX.sign && getCell(2, 1) === playerX.sign && getCell(2, 2) === playerX.sign) {
-            return endGame("PlayerX won!");
+            return endGame("Player X won!");
         }
 
         if (getCell(2, 0) === playerO.sign && getCell(2, 1) === playerO.sign && getCell(2, 2) === playerO.sign) {
-            return endGame("PlayerO won!");
+            return endGame("Player O won!");
         }
 
         if (getCell(0, 0) === playerX.sign && getCell(1, 1) === playerX.sign && getCell(2, 2) === playerX.sign) {
-            return endGame("PlayerX won!");
+            return endGame("Player X won!");
         }
 
         if (getCell(0, 0) === playerO.sign && getCell(1, 1) === playerO.sign && getCell(2, 2) === playerO.sign) {
-            return endGame("PlayerO won!");
+            return endGame("Player O won!");
         }
 
         if (getCell(0, 2) === playerX.sign && getCell(1, 1) === playerX.sign && getCell(2, 0) === playerX.sign) {
-            return endGame("PlayerX won!");
+            return endGame("Player X won!");
         }
 
         if (getCell(0, 2) === playerO.sign && getCell(1, 1) === playerO.sign && getCell(2, 0) === playerO.sign) {
-            return endGame("PlayerO won!");
+            return endGame("Player O won!");
+        }
+
+        if (getFreeCells().length === 0) {
+            return endGame("It is a tie");
         }
     }
 
-    var cells = document.querySelectorAll('.cell-board');
+    function getFreeCells() {
+       return cells.filter(function (cell) {
+            return cell.innerText === "";
+        });
+    }
+
+    function computerMove() {
+        var freeCells = getFreeCells();
+
+        var randomIndex = Math.floor(Math.random() * freeCells.length);
+        var cell = freeCells[randomIndex];
+        cell.innerText = activePlayer.sign;
+        nextPlayer(playerX, playerO);
+        checkWin();
+    }
+
+    function gameStartsWithXByPlayer() {
+        playerX.isComputer = false;
+        playerO.isComputer = true;
+        resetGame();
+    }
+
+    function gameStartsWithXByComputer() {
+        playerX.isComputer = true;
+        playerO.isComputer= false;
+        resetGame();
+        computerMove();
+    }
+
+    var player = document.querySelector('#player');
+    player.addEventListener('click', gameStartsWithXByPlayer);
+
+    var computer = document.querySelector('#computer');
+    computer.addEventListener('click', gameStartsWithXByComputer);
+
+    var cells = Array.prototype.slice.call(document.querySelectorAll('.cell-board'));
     var reset = document.querySelector('#reset');
     reset.addEventListener('click', resetGame);
 
@@ -117,6 +157,10 @@
             cell.innerText = activePlayer.sign;
             nextPlayer(playerX, playerO);
             checkWin();
+
+            if (activePlayer.isComputer && !gameEnded) {
+                computerMove();
+            }
 
         });
     })
